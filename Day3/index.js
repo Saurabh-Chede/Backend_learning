932dfd581993d2da37c0b6d4dc949c6d3920c834
Saurabh-Chede <saurabhchede21@gmail.com>
2026-04-22 19:23:30 +0530

    day3_backend

---
+++ Day3/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/saurabh", (req, res) => {
+  res.send("SAURABH");
+});
+
+app.get("/login", (req, res) => {
+  res.send("this is login page");
+});
+
+app.listen(8000, () => {
+  console.log("server is running on port 8000");
+});
