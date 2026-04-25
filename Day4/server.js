72aeb5b8210b6b84029dca710d7b2f63258b9685
Saurabh-Chede <saurabhchede21@gmail.com>
2026-04-25 19:44:30 +0530

    day4_backend

--- Day4/server.js
+++ Day4/server.js
@@ -2,13 +2,73 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+let products = [
+  { id: 1, name: "smartphone" },
+  { id: 2, name: "tv" },
+  { id: 3, name: "laptop" },
+];
+
+app.get("/product", (req, res) => {
+  res.json(products);
 });
 
+app.get("/product/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product) {
+    return res.status(404).send("Product not found");
+  }
+
+  res.json(product);
+});
+
+app.post("/product", (req, res) => {
+  const newProduct = req.body;
+
+  products.push(newProduct);
+
+  res.json({
+    message: "Product added",
+    products,
+  });
+});
+
+app.put("/product/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product) {
+    return res.status(404).send("Product not found");
+  }
+
+  product.name = name;
+
+  res.json({
+    message: "Product updated",
+    product,
+  });
+});
+
+app.delete("/product/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = products.findIndex((p) => p.id === id);
+
+  if (index === -1) {
+    return res.status(404).send("Product not found");
+  }
+
+  products.splice(index, 1);
+
+  res.send("Product deleted");
+});
 
 app.listen(8000, () => {
-  console.log("server is running on port 8000");
+  console.log("Server running on http://localhost:8000");
 });
