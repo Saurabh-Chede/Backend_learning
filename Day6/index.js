266f73a75bd2d9221bf3eb3f7cac1d19c3b3163f
Saurabh-Chede <saurabhchede21@gmail.com>
2026-05-03 09:47:50 +0530

    3/05/2026

--- Day6/index.js
+++ Day6/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import MainRouter from "./routes/index.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is working..");
@@ -9,6 +13,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1", MainRouter);
 
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Connected to MongoDB.");
+});
+
 app.listen(8000, () => {
-  console.log("Server running on port 8000.");
+  console.log("Server is running on port 8000.");
 });
