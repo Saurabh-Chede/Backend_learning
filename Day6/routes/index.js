69a63492635fa9d64c98dc6883c82859c4510503
Saurabh-Chede <saurabhchede21@gmail.com>
2026-05-07 16:40:56 +0530

    7/05/2026

--- Day6/routes/index.js
+++ Day6/routes/index.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 import AuthRouter from "./auth.route.js";
 import UserRouter from "./user.route.js";
 import AdminRouter from './admin.route.js'
+import SellerRouter from './seller.route.js'
+import ProductRouter from './product.route.js'
 
 const MainRouter = Router();
 
 MainRouter.use("/auth", AuthRouter);
 MainRouter.use("/user", UserRouter);
 MainRouter.use("/admin", AdminRouter);
+MainRouter.use("/product", ProductRouter);
+MainRouter.use("/seller", SellerRouter);
 
 export default MainRouter;
