8d4783a5a70f590b09da92d69505d77e49944810
Saurabh-Chede <saurabhchede21@gmail.com>
2026-05-14 17:54:31 +0530

    schema files formatted

--- Day6/models/product.schema.js
+++ Day6/models/product.schema.js
@@ -1,12 +1,36 @@
 import { Schema, model, mongoose } from "mongoose";
 
 const ProductSchema = new Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  category: { type: String, required: true },
-  stock: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  stock: {
+    type: Number,
+    required: true,
+  },
+
   seller: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "users",
--- Day6/models/user.schema.js
+++ Day6/models/user.schema.js
@@ -1,10 +1,27 @@
 import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "user", enum: ["user", "seller", "admin"] },
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "seller", "admin"],
+  },
 });
 
 const UserModel = model("users", UserSchema);
