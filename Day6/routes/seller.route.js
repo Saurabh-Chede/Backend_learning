5e99f107bfe23ff0bf8d6f29962a9525e7dce134
Saurabh-Chede <saurabhchede21@gmail.com>
2026-05-28 21:19:15 +0530

    28-may-2026

--- Day6/routes/seller.route.js
+++ Day6/routes/seller.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addProduct, getProducts } from "../controllers/seller.controller.js";
+import { addProduct, getProducts,sellerDashboard } from "../controllers/seller.controller.js";
 
 const SellerRouter = Router();
 
 SellerRouter.post("/add-product", addProduct);
 SellerRouter.get("/get-products", getProducts);
+SellerRouter.get("/dashboard", sellerDashboard);
 
 export default SellerRouter;
